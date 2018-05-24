e02443940efe73568a33603d8eb6156eef02a7c4
Michael Boroski <mjboroski@gmail.com>
2018-05-24 20:29:22 +0000

    automatically backed up by learn

--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,3 @@
-# Create your CostumeStore class here
+class Costume < ActiveRecord::Base[5.1]
+
+end
--- app/models/haunted_house.rb
+++ app/models/haunted_house.rb
@@ -1,1 +1,3 @@
-# Create your HauntedHouse class here
+class Costume < ActiveRecord::Base[5.1]
+
+end
