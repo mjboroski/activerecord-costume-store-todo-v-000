c718bb542ff15dfd287d7e23aaac066f2dcfbd31
Michael Boroski <mjboroski@gmail.com>
2018-05-24 20:14:06 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,12 @@
-# Create your haunted_houses migration here
+class CreateCostumes < ActiveRecord::Base[5.1]
+
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      price
+      size
+      image_url
+    end
+
+  end
+end
