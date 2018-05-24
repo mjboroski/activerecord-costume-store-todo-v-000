04acd3f7c9b205b11472d79a3e92e5db04dc56e1
Michael Boroski <mjboroski@gmail.com>
2018-05-24 20:18:32 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,9 +3,12 @@ class CreateCostumeStores < ActiveRecord::Base[5.1]
   def change
     create_table :costume_stores do |t|
       t.string :name
-      price
-      size
-      image_url
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
 
   end
